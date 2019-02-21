3b903bb1b1e248acba16964a171972f530f560c0
leila-alderman <leila.alderman@gmail.com>
2019-02-20 19:06:45 -0500

    Add getRandomDrink() to drink.service

--- src/app/service/drink.service.ts
+++ src/app/service/drink.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrinkService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRandomDrink() {
+    this.http.get('https://www.thecocktaildb.com/api/json/v1/1/random.php').subscribe(res => {
+      console.log(res);
+    });
+  }
 }
